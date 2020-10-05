e007cda611a38af50078cb6176532a81697bde21
kmit.akbar <akbarmd.mca@gmail.com>
2020-10-05 08:16:03 +0530

    mule samples upto date

---
+++ helloworld/src/main/java/helloworld/Person.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class Person {
+
+	public Person(String data){
+		
+	}
+	public static String test() {
+		return "Test msg:::";
+	}
+	
+	public String test1() {
+		return "non static Test msg:::";
+	}
+}
