6f2a5b7eb425a7ad3d7c5720e117156c08e54638
kmit.akbar <akbarmd.mca@gmail.com>
2020-10-15 07:52:06 +0530

    validations

---
+++ mule2java/src/main/java/mule2java/Test.java
@@ -0,0 +1,16 @@
+package mule2java;
+
+public class Test {
+
+	public Test(){
+		
+	}
+	public static String getMsg() {
+		return "Test";
+	}
+	
+	public String geData() {
+		return "geData";
+	}
+	
+}
